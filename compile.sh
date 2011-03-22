d2877da9af786852a2041099d24a1cd9172655e6
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-03-22 08:09:36 +0100

    Split the gcc command over multiple lines

--- compile.sh
+++ compile.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/make -f
 
 all:
-	gcc -arch i386 -mtune=i386 -march=i386 -lgobject-2.0.0 -lglib-2.0.0 -lgtk-quartz-2.0.0 -L/Applications/gedit.app/Contents/Resources/lib/ -DHAVE_CONFIG_H -I. `pkg-config --cflags gedit-2.20` src/scrollpaste-plugin.c -Wl,-undefined -Wl,dynamic_lookup -bundle -o libscrollpaste.so
+	gcc -arch i386 -mtune=i386 -march=i386 \
+	  -DHAVE_CONFIG_H -I. `pkg-config --cflags gedit-2.20` \
+	  -lgobject-2.0.0 -lglib-2.0.0 -lgtk-quartz-2.0.0 -L/Applications/gedit.app/Contents/Resources/lib/ \
+	  -Wl,-undefined -Wl,dynamic_lookup -bundle \
+	  -o libscrollpaste.so src/scrollpaste-plugin.c
 	cp libscrollpaste.so /Applications/gedit.app/Contents/Resources/lib/gedit-2/plugins/
 
