442bea4ea51f0fc6704729cf13ac1956a27252fe
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-03-22 08:03:59 +0100

    Convert compile.sh to a makefile

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
-#/!/bin/sh
+#!/usr/bin/make -f
 
-gcc -arch i386 -mtune=i386 -march=i386 -lgobject-2.0.0 -lglib-2.0.0 -lgtk-quartz-2.0.0 -L/Applications/gedit.app/Contents/Resources/lib/ -DHAVE_CONFIG_H -I$PWD -I$PWD `pkg-config --cflags gedit-2.20` src/scrollpaste-plugin.c -Wl,-undefined -Wl,dynamic_lookup -bundle -o libscrollpaste.so
-cp libscrollpaste.so /Applications/gedit.app/Contents/Resources/lib/gedit-2/plugins/
+all:
+	gcc -arch i386 -mtune=i386 -march=i386 -lgobject-2.0.0 -lglib-2.0.0 -lgtk-quartz-2.0.0 -L/Applications/gedit.app/Contents/Resources/lib/ -DHAVE_CONFIG_H -I. `pkg-config --cflags gedit-2.20` src/scrollpaste-plugin.c -Wl,-undefined -Wl,dynamic_lookup -bundle -o libscrollpaste.so
+	cp libscrollpaste.so /Applications/gedit.app/Contents/Resources/lib/gedit-2/plugins/
 
